cea20fff2e18d9739f902f32d1731fd558ef53a6
Venkateswarabalaji <your.email@example.com>
2023-07-10 16:35:13 +0800

    https://github.com/VenkateswaraBalaji/B47-FullStackCodePractices

---
+++ DOM-Day1/script.js
@@ -0,0 +1,12 @@
+
+document.getElementById("heading").style.color ="red";
+
+document.getElementById("description").style.color = "blue";
+document.getElementById("description").innerHTML = "Hello World";
+
+const inputs = Array.from(document.getElementsByTagName("input"))
+
+inputs.forEach((Element,index) => {
+    if (index === 0){
+        element.style.marginbottom = 50 + "px" }
+    console.log(element.value)});
